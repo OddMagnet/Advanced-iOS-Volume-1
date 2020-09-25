11105c04c2d0a2454a6dc5cfdb2adedfeab457a2
OddMagnet <mibruenen@gmail.com>
2020-09-25 16:11:08 +0200

    Initial commit

---
+++ Happy Days/Happy Days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happy Days
+//
+//  Created by Michael Brünen on 25.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
