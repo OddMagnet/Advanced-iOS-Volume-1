6d7ec561caa961c2e954187b88de99464fcc85aa
OddMagnet <mibruenen@gmail.com>
2020-09-26 23:44:09 +0200

    Finish main interface

---
+++ Happy Days/Happy Days/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  Happy Days
+//
+//  Created by Michael Brünen on 26.09.20.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
