007dcd2321bc355b707dd077b20d5e8caa4e39d7
OddMagnet <mibruenen@gmail.com>
2020-10-03 16:46:55 +0200

    Initial commit

---
+++ 1 - Happy Days (SwiftUI)/Happy Days/Happy Days/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Happy Days
+//
+//  Created by Michael Brünen on 03.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1 - Happy Days (SwiftUI)/Happy Days/Happy Days/Happy_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_DaysApp.swift
+//  Happy Days
+//
+//  Created by Michael Brünen on 03.10.20.
+//
+
+import SwiftUI
+
+@main
+struct Happy_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
