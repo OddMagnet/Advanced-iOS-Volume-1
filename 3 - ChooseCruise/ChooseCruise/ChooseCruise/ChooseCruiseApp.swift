70b50bc9b3070f3bd3ab09cef339a348732aee4d
OddMagnet <mibruenen@gmail.com>
2020-12-11 14:05:57 +0100

    Initial commit

---
+++ 3 - ChooseCruise/ChooseCruise/ChooseCruise/ChooseCruiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseCruiseApp.swift
+//  ChooseCruise
+//
+//  Created by Michael Brünen on 11.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ChooseCruiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
